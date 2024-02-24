86c3072cc0690201785db94328ce272bb2bef4ee
Rynare <rynare.of.legend@gmail.com>
2024-02-24 13:58:54 +0700

    menambahkan author profile

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.body.offsetWidth <= 600) {
         const hamburgerStatus = document.querySelector('#hamburger-status');
         const navMobile = document.querySelector('.nav-mobile');
+        const navMain = document.querySelector('header')
+
         hamburgerStatus.addEventListener("change", () => {
             if (hamburgerStatus.checked == true) {
                 navMobile.classList.contains('hidden') ? navMobile.classList.remove('hidden'):""
@@ -13,6 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 }, 500);
             }
         })
-    
+
+        if (document.body.offsetWidth <= 360) {
+            const asideProfile = document.querySelector('aside')
+            const profileMobileBtn = document.querySelector('aside .profile-mobile')
+            const asideHiddenOffset = asideProfile.offsetWidth - profileMobileBtn.offsetWidth + 2
+            asideProfile.style.top = navMain.offsetHeight + 10 + 'px'
+            asideProfile.style.right = '-'+ asideHiddenOffset + 'px'
+
+            profileMobileBtn.addEventListener('click', () => {
+                profileMobileBtn.classList.contains('show') ? profileMobileBtn.classList.remove('show') : profileMobileBtn.classList.add('show')
+                profileMobileBtn.classList.contains('show') ? asideProfile.style.right = 0 + 'px' : asideProfile.style.right = '-'+ asideHiddenOffset + 'px'
+            })
+        }
     }
 })
