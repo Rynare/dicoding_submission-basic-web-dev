4b09358cd756029acfa51b3b97585ca1bb506786
Rynare <rynare.of.legend@gmail.com>
2024-02-25 16:17:14 +0700

    menambahkan layout responsive

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,46 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
   const mainHeader = document.querySelector('header')
   const navMain = document.querySelector('header .nav-main')
+  const getBrowserWidth = () => document.body.offsetWidth;
+  const hamburgerStatus = document.querySelector('#hamburger-status');
 
-  if (document.body.offsetWidth <= 600) {
-    const hamburgerStatus = document.querySelector('#hamburger-status');
-    const navMobile = document.querySelector('.nav-mobile');
+  allEventListenerToggle()
 
-    hamburgerStatus.addEventListener("change", () => {
-      if (hamburgerStatus.checked) {
-        navMobile.style.display = 'block'
-        navMobile.classList.add('show')
-      } else if (hamburgerStatus.checked == false) {
-        navMobile.classList.add('hidden')
-        setTimeout(() => {
-          navMobile.style.display = "none"
-          navMobile.classList.remove('show')
-          navMobile.classList.remove('hidden')
-        }, 500);
-      }
-    })
+  window.addEventListener('resize', () => {
+    allEventListenerToggle()
+  })
 
-    if (document.body.offsetWidth <= 360) {
-      const asideProfile = document.querySelector('aside')
-      const profileMobileBtn = document.querySelector('aside .profile-mobile')
-      const asideHiddenOffset = asideProfile.offsetWidth - profileMobileBtn.offsetWidth + 2
-      asideProfile.style.top = mainHeader.offsetHeight + 52 + 10 + 'px'
-      asideProfile.style.right = '-' + asideHiddenOffset + 'px'
-
-      profileMobileBtn.addEventListener('click', () => {
-        profileMobileBtn.classList.contains('show') ? profileMobileBtn.classList.remove('show') : profileMobileBtn.classList.add('show')
-        profileMobileBtn.classList.contains('show') ? asideProfile.style.right = 0 + 'px' : asideProfile.style.right = '-' + asideHiddenOffset + 'px'
-      })
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= mainHeader.offsetHeight / 2) {
+      mainHeader.classList.contains('scrolled') ? '' : mainHeader.classList.add('scrolled')
+    } else {
+      mainHeader.classList.contains('scrolled') ? mainHeader.classList.remove('scrolled') : ''
     }
+  })
 
-    window.addEventListener('scroll', () => {
-      if (window.scrollY >= mainHeader.offsetHeight / 2) {
-        mainHeader.classList.contains('scrolled') ? '' : mainHeader.classList.add('scrolled')
-      } else {
-        mainHeader.classList.contains('scrolled') ? mainHeader.classList.remove('scrolled') : ''
-      }
-    })
+  function allEventListenerToggle() {
+    asideProfileBtnEventToggle()
+    hamburgerBtnEventToggle()
   }
 
+  function hamburgerBtnOnClick() {
+    const navMobile = document.querySelector('.nav-mobile');
+    if (hamburgerStatus.checked) {
+      navMobile.style.display = 'block'
+      navMobile.classList.add('show')
+    } else {
+      navMobile.classList.add('hidden')
+      setTimeout(() => {
+        navMobile.style.display = "none"
+        navMobile.classList.remove('show')
+        navMobile.classList.remove('hidden')
+      }, 500);
+    }
+  }
 
+  function hamburgerBtnEventToggle(params) {
+    if (getBrowserWidth() <= 760) {
+      hamburgerStatus.addEventListener("change", hamburgerBtnOnClick)
+    } else {
+      hamburgerStatus.removeEventListener('change', hamburgerBtnOnClick)
+    }
+  }
+
+  function asideProfileBtnOnClick() {
+    const asideProfile = document.querySelector('aside')
+    asideProfile.classList.toggle('show-mobile')
+  }
+
+  function asideProfileBtnEventToggle() {
+    const profileMobileBtn = document.querySelector('aside .profile-mobile')
+    if (getBrowserWidth() <= 360) {
+      profileMobileBtn.addEventListener('click', asideProfileBtnOnClick)
+    } else {
+      profileMobileBtn.removeEventListener('click', asideProfileBtnOnClick)
+    }
+  }
 })
